0e455782daf8633b6ef86daed0517d2c1d813c08
Zahed75 <Zahedhasan.CS@gmail.com>
2021-06-01 14:49:37 +0600

    login page done

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-$('.owl-carousel').owlCarousel({
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
     loop:true,
     margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
 })
